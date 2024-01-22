ae36da87642df5c2b86dc774379607c05b960614
Ohlomonchick <proskurin.dima16@gmail.com>
2024-01-22 21:57:28 +0300

    simple flag validation

--- interface/views.py
+++ interface/views.py
@@ -10,5 +10,13 @@ class LabDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["form"] = LabAnswerForm()
+        context["submitted"] = False
+        ans = self.request.GET.get("answer_flag")
+        if ans:
+            if ans == context["object"].answer_flag:
+                context["submitted"] = True
+            else:
+                context["form"].fields["answer_flag"].label = "Неверный флаг!"
+
         return context
 
