444e33c2821aed553b315e9a1234737010eef95e
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2024-02-16 05:04:13 -0500

    Registration for random users

--- Cyberpolygon/urls.py
+++ Cyberpolygon/urls.py
@@ -18,9 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from interface.views import registration
 
 urlpatterns = [
+    path('', registration),
     path('jet/', include('jet.urls', 'jet')),
     path('cyberpolygon/', include(('interface.urls', 'interface'), namespace='interface')),
     path('admin/', admin.site.urls),
